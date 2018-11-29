79a7fc8e5c3231f36b0c1caecbe59f427b3eb414
briannoogin <brian.qh.nguyen@gmail.com>
2018-11-29 14:27:06 -0600

    got white background to appear

--- project.asm
+++ project.asm
@@ -14,20 +14,22 @@
 # Do 10 iterations of k-means
 .data
 # screen size dimensions
-screenHeight: .word 64
-screenWidth: .word 64
+displayHeight: .word 64
+displayWidth: .word 64
 # color of classes
 blue: 	.word	0x0066cc # color of 1 class
 red: .word  0xFF0000 # color of 0 class
-backgroundColor:.word	0xFFFFFF # white 
+displayColor:.word 0xFFFFFF # white 
+black: .word 0x000000 # color of default class
 # data file
 dataFile: .asciiz "data.txt"
-# 100X2 table
-xDimension: .space 100
-yDimension: .space 100
+# 100X3 table
+xVector: .space 100
+yVector: .space 100
+colorVector: .space 100
 fileBuffer: .space 400
 .text
-	main:
+main:
 	### Read in text data ###
 	# open file
 	li $v0, 13 
@@ -50,24 +52,41 @@ fileBuffer: .space 400
 	
 	# output character
 	la $t1, fileBuffer # store address of the file buffer
-	la $t2, xDimension # store address of 1st column of table
-	la $t3, yDimension # store address of 2nd column of table
+	la $t2, xVector # store address of 1st column of table
+	la $t3, yVector # store address of 2nd column of table
+	la $t4, colorVector # stores address of 3rd column of table
 	li $s1, 400
 	li $s2, 0 # store character count to stop the loop
-	loopThroughBuffer:
-	addi $t4, $t1, 2 # store address of 2nd number
+	lw $s3, black # store the color of black
+loopThroughBuffer:
+	addi $t5, $t1, 2 # store address of 2nd number
 	sb $t1, ($t2) # store number in x column
-	sb $t4, ($t3) # store number in y column
+	sb $t5, ($t3) # store number in y column
+	sb $s3 ($t4) # store color in color column
 	addi $t2, $t2, 1 # increment column index by 1
 	addi $t3, $t3, 1
+	addi $t4, $t4, 4 # move color vector to next word
 	addi $t1, $t1, 4 # increment buffer by three to move to new line
 	addi $s2, $s2, 4 # increment number of characters read
 	bne $s1, $s2, loopThroughBuffer
 	### Draw in graph ###
+	lw $a0, displayWidth
+	lw $a1, displayColor
+	mul $a2, $a0,$a0 # calculate total pixels
+	mul $a2, $a2, 4 # each pixel stores 4 bytes
+	add $a2, $a2, $gp # add to the $gp to mark the end
+	addi $a3, $gp, 0 # loop counter
+# makes the screen white
+FillScreen:
+	sw $a1, ($a3) # change pixel to white
+	addiu $a3, $a3, 4 # move word by 1
+	bne $a2, $a3, FillScreen
+# draws the graph lines
+DrawGraph:
 	### Iterate ###
 	### K-means Cluster ##
 	
 	# exit the program 
-	exit:
-		li $v0, 10
-		syscall
+exit:
+	li $v0, 10
+	syscall
