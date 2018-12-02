1cedd06efb8367a4a96c857fcd3f9a740ec3a859
briannoogin <brian.qh.nguyen@gmail.com>
2018-12-02 14:21:26 -0600

    refactored some code and added comments and spacing

--- project.asm
+++ project.asm
@@ -1,6 +1,6 @@
-# K-means clustering visualization for binary classifcation data
-# by Brian Nguyen
-# 	Program uses Bitmap display to display the scatter plot
+# K-means clustering visualization for binary classifcation data     #
+# by Brian Nguyen						     #	
+# 	Program uses Bitmap display to display the scatter plot      #
 #	Bitmap Display Settings:                                     #
 #	Unit Width: 8						     #
 #	Unit Height: 8						     #
@@ -15,20 +15,25 @@
 # print output of centroids to console 
 # adds tests for convergence
 .data
+
 # screen size dimensions
 displayHeight: .word 64
 displayWidth: .word 64
+
 # number of iterations for k-means
 iterations: .word 10
+
 # color of classes
 blue: 	.word	0x0066cc # color of 1 class
 red: .word  0xFF0000 # color of 0 class
 displayColor:.word 0xFFFFFF # white 
 black: .word 0x000000 # color of default class
+
 # data file
 dataFile: .asciiz "data.txt"
+
 # 100X3 table
-# align 1 makes aligns memory to half word
+# align 2 makes aligns memory to word
 xVector:.align 2 
 	.space 400 
 yVector:.align 2 
@@ -37,15 +42,14 @@ colorVector:.align 2
 	    .space 400
 fileBuffer: .align 0
 	    .space 600 
+	    
 centroids: .space 16 # array contains the coordinates of both centroids 
 
+# used for floating point division 
 division: .float 2.0
 guess: .float 2
-parameter: .word 2
 .text
 main:
-lw $a0, parameter
-jal calculateSquareRoot
 ### Read in text data ###
 # open file
 li $v0, 13 
@@ -66,7 +70,7 @@ syscall
 li $v0,16 
 syscall
 	
-# output character
+# initialize variables 
 la $t1, fileBuffer # store address of the file buffer
 la $t2, xVector # store address of 1st column of table
 la $t3, yVector # store address of 2nd column of table
@@ -78,32 +82,44 @@ lw $s3, black # store the color of black
 loopThroughBuffer:
 	addi $t5, $t1, 3 # store address of 2nd number in the row
 	
-	lb $t6, ($t1) # load half the number from the buffer
+	# load half the number from the buffer
+	lb $t6, ($t1) 
 	lb $t7, ($t5) 
-	addi $t6,$t6,-48 # converts ascii to number
+	
+	# converts ascii to number
+	addi $t6,$t6,-48 
 	addi $t7,$t7,-48
 	
-	lb $t8, 1($t1) # load the 2nd half of the number from the buffer
+	# load the 2nd half of the number from the buffer
+	lb $t8, 1($t1)
 	lb $t9, 1($t5)
-	addi $t8,$t8,-48 # converts ascii to number
+	
+	# converts ascii to number
+	addi $t8,$t8,-48
 	addi $t9,$t9,-48
 	
-	mul $t6, $t6, 10 # multiply the digit by 10 to make the num the tens digit
+	# multiply the digit by 10 to make the num the tens digit
+	mul $t6, $t6, 10 
 	mul $t7, $t7, 10
-	add $t6, $t6, $t8 # combine the tens and one digit 
+	
+	# combine the tens and one digit 
+	add $t6, $t6, $t8 
 	add $t7, $t7, $t9
 	
 	sw $t6, ($t2) # store number in x column
 	sw $t7, ($t3) # store number in y column
 	sw $s3 ($t4) # store color in color column
 	
-	addi $t2, $t2, 4 # increment column index by a word
+	# increment column index by a word
+	addi $t2, $t2, 4 
 	addi $t3, $t3, 4
 	addi $t4, $t4, 4 
 	
-	addi $t1, $t1, 6 # increment buffer by four to move to new line
+	addi $t1, $t1, 6 # increment buffer by six to move to new line
 	addi $s2, $s2, 6 # increment number of characters read
-	bne $s1, $s2, loopThroughBuffer
+	
+	# i < 400
+	blt $s2, $s1, loopThroughBuffer 
 	
 ### Draw in graph ###
 lw $a0, displayWidth
@@ -125,32 +141,42 @@ mul $a2,$a0, 4 # multiply the number of pixels by 4 to format the address
 mul $a2, $a2, 35 # find total length of a column
 add $a2, $a2, $gp # add length of column by the position of the graph
 move $a3, $gp # reset to index original position 
+
 # go down to draw the y axis
 DrawYAxis: 
 	sw $a1, ($a3)
+	# move to next row
 	addi $a3, $a3, 256
-	bne $a3, $a2, DrawYAxis
+	blt $a3, $a2, DrawYAxis
 # go right to draw the x axis
 addi $a2, $a2, 256
 DrawXAxis:
 	sw $a1, ($a3)
+	# move one pixel right 
 	addi $a3, $a3, 4
-	bne $a3, $a2, DrawXAxis
+	blt $a3, $a2, DrawXAxis
 ### Plot Points ###
 la $s1, xVector 
 la $s2, yVector
 la $s3, colorVector
 li $s4,0 # i = 0
+
 drawAllPoints:
+	# load the points
 	lw $a0, ($s1)
 	lw $a1, ($s2)
 	lw $a2, ($s3)
 	jal drawPoint
-	addi $s1, $s1, 4 # move to next word 
+	
+	# move to next word 
+	addi $s1, $s1, 4 
 	addi $s2, $s2, 4
 	addi $s3, $s3, 4
+	
+	# i++
 	addi $s4, $s4, 1
-	bne $s4, 100, drawAllPoints
+	# i < 100
+	blt $s4, 100, drawAllPoints
 ### K-means Clustering ###
 la $s1, xVector 
 la $s2, yVector
@@ -158,33 +184,47 @@ la $s3, colorVector
 la $s4, centroids
 lw $s5, blue
 lw $s6, red
+
 # take the first two points as the centroids 
 sw $s1, ($s4) # store 1st centroid
 sw $s2, 4($s4)
 sw $s5, ($s3) # mark point as blue
+
 addi $t1, $s1, 4 # get second point
 addi $t2, $s2, 4
 sw $t1, 8($s4) # store 2nd centroid 
 sw $t2, 12($s4)
 sw $s6, 4($s3)
+
 # print the newly colored points
 lw $a0, ($s1)
 lw $a1, ($s2)
 lw $a2, ($s3)
 jal drawPoint
+
+# pause the program for .1 seconds
+li $v0, 32
+li $a0, 100 
+syscall
+
 lw $a0, 4($s1)
 lw $a1, 4($s2)
 lw $a2, 4($s3)
 jal drawPoint
-# pause the program for .1 seconds so visualization can be easily seen
+
+# pause the program for .1 seconds
 li $v0, 32
 li $a0, 100 
 syscall
+
 ### Iteration ###
 li $t1, 0 # index i = 0
 lw $t2, iterations
 iteration:
-
+	
+	# pause the program for .1 seconds 
+	li $v0, 32
+	li $a0, 100 
 	addi $t1,$t1, 1 
 	bne $t1,$t2, iteration 
 
@@ -207,27 +247,45 @@ drawPoint:
 	sub $t1, $t1, $t2 # offset the address to correctly match the y coordinate 
 	mul $t2, $a0, 4 # multiply x coordinate by 4 to get the location of the x address 
 	add $t1, $t1, $t2  # add to move point right 
+	
 	# change address to specified color 
 	sw $a2,($t1)
 	jr $ra
 	 
 # input: $a0: x of first data point, $a1: y of first data point, $a2: x of second data point, $a3: y of second data point 
 # output: $v0: returns floating point and represents distance between two points
+# distance formula = ((x2-x1)^2 + (y2-y1)^2)^.5
 calculateEuclideanDistance:
+	# x2 - x1
+	#add $v0, $a0, $a2
+	# 
 	jr $ra
 # input: $a0: integer number
 # output: $f4: returns the resulting floating square root of the number
 # square root is approximated with 3 iterations of Newton's Method. 
 # Newton's method: xn+1 = .5(xn + a/xn) where a is number you want to take the square root of and x0 is the initial guess
 calculateSquareRoot:
+	# initialize variables 
 	mtc1 $a0, $f2 # move input to floating point register
 	cvt.s.w $f2, $f2 # convert word to single point
 	li $t8, 0 # num iterations = 0
 	li $t9, 5
 	l.s $f3, division # used for division 
+	li $t0, 0 # i = 0
+	srl $t2, $a0, 1
+	move $t1, $a0, # store a into x
+	
 	calcInitialGuess: # calculate the integer square root for a close guess
-		#sll $t1, $t2, $
-		l.s $f1, guess # guess is 2 for now
+		#l.s $f1, guess # guess is 2 for now
+		divu $t3, $a0, $t1  # a/x
+		add  $t3, $t1, $t3 	# x + a/x
+		srl $t1, $t3, 1 # x = .5(x + a/x)
+		addi $t0, $t0,1
+		blt $t0, $t2, calcInitialGuess # i < a/2
+		
+	mtc1 $t1,$f1 # move x to floating point register
+	cvt.s.w $f1, $f1 # convert word to single point
+	
 	approximate:
 		div.s $f4, $f2, $f1 # a/xn
 		add.s $f4, $f1, $f4 # xn + a/xn
